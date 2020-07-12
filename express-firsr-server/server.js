2cf98ccbe0ad6b75b7f2996e731350ce9fb23890
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-07-12 23:21:30 +0530

    googlesearch  added

--- express-firsr-server/server.js
+++ express-firsr-server/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World')
+    let name = 'Guest'
+    if (req.query.user)
+        name = req.query.user
+    res.send('Hello World ' + name)
 })
 
 app.use('/abc', express.static(__dirname + '/public')) //
